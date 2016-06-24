722b2b8657a4c9731f49e316cfe9da5ec65d22d9
Ian Clark <ian@ianleeclark.com>
2016-06-23 21:53:15 -0500

    Issue #15
    ADD:
      - lru_cache.go Is mostly completed, just need to add many many tests and
        verify that there's no race conditions (jokes on me, there are).
      - lru_cache_test.go Working on adding more unit tests.

--- lib/lru_cache/lru_cache.go
+++ lib/lru_cache/lru_cache.go
@@ -2,8 +2,10 @@ package olilib_lru
 
 
 import (
-
+        "time"
 )
+
+var MAXINT64 = int64(1 << 63 - 1)
 
 // LRUCache is a simple implementation of an LRU cache which will be used in
 // the cache based whenever we want to cache values that we don't care too much
@@ -11,24 +13,62 @@ import (
 // keys are preserved within the datastructure.
 type LRUCache struct {
        KeyCount int
-       Keys []interface{}
+       Keys map[string]string
+       KeyTimeouts map[string]int64
 }
 
+// New simply allocates a new instance of an LRU cache with `maxEntries` total
+// slots.
 func New(maxEntries int) *LRUCache {
         return &LRUCache{
                 KeyCount: maxEntries,
-                Keys: make([]interface{}, maxEntries),
+                Keys: make(map[string]string),
+                KeyTimeouts: make(map[string]int64),
         }
 }
 
-func (l *LRUCache) Add(key interface{}) {
+func (l *LRUCache) Add(key string, value string) (string, error) {
+        value, keyExists := l.Keys[key]
+        if keyExists {
+                l.KeyTimeouts[key] = getCurrentUnixTime()
+                return value, nil
+        }
 
+        if(len(l.Keys) >= l.KeyCount) {
+                l.RemoveLeastUsed()
+        }
+
+        l.Keys[key] = value
+        l.KeyTimeouts[key] = getCurrentUnixTime()
+
+        return key, nil
 }
 
-func (l *LRUCache) Get(key interface{}) {
+func (l *LRUCache) Get(key string) (string, error) {
+        value, keyExists := l.Keys[key]
+        if keyExists {
+                l.KeyTimeouts[key] = getCurrentUnixTime()
+        }
 
+        return value, nil
 }
 
 func (l *LRUCache) RemoveLeastUsed() {
+        var lowest int64
+        var lowestKey string
 
+        lowest = MAXINT64
+
+        for k := range l.KeyTimeouts {
+                if l.KeyTimeouts[k] < lowest {
+                        lowestKey = k
+                }
+        }
+
+        delete(l.Keys, lowestKey)
+        delete(l.KeyTimeouts, lowestKey)
+}
+
+func getCurrentUnixTime() int64 {
+        return time.Now().Unix()
 }
--- lib/lru_cache/lru_cache_test.go
+++ lib/lru_cache/lru_cache_test.go
@@ -4,6 +4,60 @@ import (
         "testing"
 )
 
+var TESTLRU = New(10)
+
 func TestNew(t *testing.T) {
-        
+        expectedReturn := &LRUCache{
+                10,
+                make(map[string]string, 10),
+                make(map[string]int64, 10),
+        }
+
+        result := New(10)
+
+        if expectedReturn.KeyCount != result.KeyCount {
+                t.Fatalf("Expected %v, got %v", expectedReturn.KeyCount, result.KeyCount)
+        }
+}
+
+func TestAdd(t *testing.T) {
+        key, err := TESTLRU.Add("Key", "Value")
+
+        if key != "Key" {
+                t.Fatalf("Failed adding a key to the LRU cache")
+        }
+
+        if err != nil {
+                t.Fatalf("Weird things happened adding a key")
+        }
+}
+
+func TestAddRemoveOldest(t *testing.T) {
+        testLRU := New(3)
+        testLRU.Add("Key1", "value1")
+        testLRU.Add("Key2", "value2")
+        testLRU.Add("Key3", "value3")
+
+        expectedKeys := []string{"Key1", "Key2", "Key3"}
+
+        for i := range expectedKeys {
+                if _, ok := testLRU.Keys[expectedKeys[i]]; !ok {
+                        t.Fatalf("Key %v not found in the test LRU (Keys)", expectedKeys[i])
+                }
+
+                if _, ok := testLRU.KeyTimeouts[expectedKeys[i]]; !ok {
+                        t.Fatalf("Key %v not found in the test LRU (KeyTimeouts)", expectedKeys[i])
+                }
+        }
+}
+
+func TestRemoveLeastUsed(t *testing.T) {
+
+}
+
+func TestGet(t *testing.T) {
+
+}
+
+func TestGetDoesntExist(t *testing.T) {
 }
