53d00b9757b539c1a0062c81110e77e45a54c098
Ian C <ilc.ianclark@gmail.com>
2016-07-29 21:08:23 -0500

    Issue #75
    ADD:
      - cache.go Adding a copy-on-write `readCache` to allow reads without
        locking the cache.

---
+++ cache/README.md
@@ -0,0 +1,5 @@
+## Cache
+
+This is where operations for the cache reside. Mostly handles setting/getting
+values. Honestly, it's a super simple and ugly way of doing it and needs to be
+handled better in the future.
--- cache/cache.go
+++ cache/cache.go
@@ -2,30 +2,56 @@ package cache
 
 import (
 	"fmt"
+	"sync"
 )
 
 // TODO(ian): Replace this with something else
+// Cache is actually just a map[string]string. Don't tell anyone.
 type Cache struct {
+	sync.Mutex
 	Cache *map[string]string
+	readCache *map[string]string
 }
 
+// NewCache creates a new cache and internal readCache.
 func NewCache() *Cache {
 	cacheMap := make(map[string]string)
+	writeCache := make(map[string]string)
 	return &Cache{
 		&cacheMap,
+		&writeCache,
 	}
 }
 
+// Get handles retrieving a value by its key from the internal cache. It reads
+// from the readCache which is for copy-on-write optimizations so that
+// reading doesn't lock the cache.
 func (c *Cache) Get(key string) (string, error) {
-	var value string = ""
-	if value, ok := (*c.Cache)[key]; !ok {
+	var value string
+	if value, ok := (*c.readCache)[key]; !ok {
 		return value, fmt.Errorf("Key not found in cache")
 	}
 
 	return value, nil
 }
 
+
+// copyCache handles creating a copy of the cache
+func (c *Cache) copyCache() {
+	c.Lock()
+	for k, v := range c.Cache {
+		c.readCache[k] = v
+	}
+	c.Unlock()
+}
+
+// Set handles adding a key/value pair to the cache and updating the internal
+// readCache.
 func (c *Cache) Set(key string, value string) error {
+	c.Lock()
 	(*c.Cache)[key] = value
+	c.Unlock()
+	go c.copyCache()
+
 	return nil
 }
