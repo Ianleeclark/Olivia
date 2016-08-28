64b47a97809b1ef2e62df068e330c46d4104153f
Ian Clark <ian@ianleeclark.com>
2016-08-28 17:52:32 -0500

    Issue #124
    ADD:
      - bloomfilter/bloom.go:calculateHash() Now uses murmur3 and jenkins
        non-crypto hashes instead of just FNV + offset for adding keys into the
        bloom filter.

--- build/install_deps.bat
+++ build/install_deps.bat
@@ -1,2 +1,4 @@
 go get github.com/spf13/viper
 go get github.com/willf/bitset
+go get github.com/spaolacci/murmur3
+go get github.com/mtchavez/jenkins
--- build/install_deps.sh
+++ build/install_deps.sh
@@ -2,3 +2,5 @@
 
 go get github.com/spf13/viper
 go get github.com/willf/bitset
+go get github.com/spaolacci/murmur3
+go get github.com/mtchavez/jenkins
