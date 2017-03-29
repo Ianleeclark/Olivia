2e0006bfd8d8a8e19669f4f2911b68736b498e30
Ian Clark <ian@ianleeclark.com>
2017-03-28 19:32:05 -0500

    Issue #156
    CHANGE:
      - language_map_test.go Editing file to respect cache's internal Bloomfilter now. Resolving build failures.

--- network/incoming/language_map_test.go
+++ network/incoming/language_map_test.go
@@ -83,9 +83,16 @@ func TestRequestBloomFilter(t *testing.T) {
 	bf.AddKey([]byte("key3"))
 	bf.AddKey([]byte("key4"))
 
+	testCache := cache.NewCache(nil, CONFIG)
+	testCache.Set("key1", "value1")
+	testCache.Set("keyalksdjfl", "value1")
+	testCache.Set("key2", "value2")
+	testCache.Set("key3", "value2")
+	testCache.Set("key4", "value3")
+
 	ctx := &ConnectionCtx{
 		nil,
-		nil,
+		testCache,
 	}
 
 	command := parser.CommandData{"hash", "REQUEST", map[string]string{"bloomfilter": ""}, make(map[string]string), nil}
@@ -97,7 +104,7 @@ func TestRequestBloomFilter(t *testing.T) {
 	requestData, _ := parser.NewParser(nil).Parse(newBfStr, nil)
 
 	var bfToParse string
-	for k, _ := range requestData.Args {
+	for k := range requestData.Args {
 		bfToParse = k
 		break
 	}
